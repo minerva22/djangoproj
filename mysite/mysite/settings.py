d6f18e251ce88b99000d83979d36b2c9c12c1208
minerva22 <minerva.moawad@gmail.com>
2017-04-07 09:15:08 +0300

    Customize the admin form,css style and Test Unit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -99,6 +99,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
